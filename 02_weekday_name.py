18c622e659e7ed68d0099fdd821cde17961e283d
m-zz <marcusyzhu@gmail.com>
2021-03-01 15:30:09 -0800

    01-05 complete

--- 02_weekday_name.py
+++ 02_weekday_name.py
@@ -12,3 +12,10 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    dict = {1: "Sunday", 2: "Monday", 3: "Tuesday", 4: "Wednesday", 5: "Thursday",
+    6: "Friday", 7: "Saturday"}
+
+    if day_of_week in dict:
+        return dict[day_of_week]
+    else:
+        return None
